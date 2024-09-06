21f8a45504f832938893772e7ede2958eb542dd9
Rahul22b <168935427+Rahul22b@users.noreply.github.com>
2024-09-06 14:42:55 +0530

    Update Author.jsx

--- components/Author.jsx
+++ components/Author.jsx
@@ -5,6 +5,6 @@ import { context } from '../context/Context'
 export default function Author() {
   const [isDark]=useContext(context)
   return (
-    <div className={`author ${isDark}`}>Made with ❤️ by Itom</div>
+    <div className={`author ${isDark}`}>Made with ❤️ by Rahul with th ehelp of Anurag Bhaiya</div>
   )
 }
